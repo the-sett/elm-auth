f00c0f63e2c1589346942dc6cf85f188c69e4c9d
Rupert Smith <rupert@thesett.com>
2020-05-22 18:57:05 +0100

    Added save and restore and failure reasons.

--- src/AuthAPI.elm
+++ src/AuthAPI.elm
@@ -19,6 +19,7 @@ will supply an appropriate challenge type and commands to answer challenges.
 -}
 
 import Http
+import Json.Encode exposing (Value)
 
 
 {-| Username and password credentials.
@@ -40,17 +41,29 @@ what access rights the authenticated user has. This might typically be used to
 only render parts of the UI that are going to be able to work correctly when a
 user has certain permissions.
 
+`saveState` provides a JSON serialized snapshot of the authenticated state. This
+can be used with the `AuthAPI.restore` function to attempt to re-create the
+authenticated state without logging in again. For example, put the save state in
+local storage, where a new instance of the application can pick it up and carry
+on in the authenticated state. Be aware that the save state will contain sensitive
+information such as access tokens - so think carefully about the security
+implications of where you put it.
+
 -}
 type alias AuthInfo auth =
-    { auth | scopes : List String, subject : String }
+    { auth
+        | scopes : List String
+        , subject : String
+        , saveState : Value
+    }
 
 
 {-| The visible status of the authentication model.
 -}
-type Status auth chal
+type Status auth chal fail
     = LoggedOut
     | LoggedIn (AuthInfo auth)
-    | Failed
+    | Failed fail
     | Challenged chal
 
 
@@ -66,13 +79,14 @@ to a common pattern. This standardizes how authentcation is handled in
 applications.
 
 -}
-type alias AuthAPI config model msg auth chal ext =
+type alias AuthAPI config model msg auth chal ext fail =
     { ext
         | init : config -> Result String model
+        , restore : Value -> Result String model
         , login : Credentials -> Cmd msg
         , logout : Cmd msg
         , unauthed : Cmd msg
         , refresh : Cmd msg
-        , update : msg -> model -> ( model, Cmd msg, Maybe (Status auth chal) )
+        , update : msg -> model -> ( model, Cmd msg, Maybe (Status auth chal fail) )
         , addAuthHeaders : model -> List Http.Header -> List Http.Header
     }
